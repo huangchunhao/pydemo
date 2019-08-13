54b4441b5f3a170c6b4d53c05bb845844cff90e1
huangchunhao <2015huangchunhao>
2019-08-13 20:24:25 +0800

    decorator

---
+++ demo_decorator/decoratored.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2019/8/13 0013 20:08
+# @Author  : Vincent
+# @Email   : Vincent@163.com
+# @File    : decoratored.py
+# @Software: PyCharm
+
+from demo_decorator.decoratortest import *
+
+@de1
+def use1():#被装饰的函数不带形参
+    print("use1")
---
+++ demo_decorator/decoratortest.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2019/8/13 0013 20:07
+# @Author  : Vincent
+# @Email   : Vincent@163.com
+# @File    : decoratortest.py
+# @Software: PyCharm
