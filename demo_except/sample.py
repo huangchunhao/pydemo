33901f28cbda87f5a323114b196595c82c8718d2
huangchunhao <2015huangchunhao>
2019-08-18 21:30:45 +0800

    except

--- demo_except/sample.py
+++ demo_except/sample.py
@@ -4,3 +4,23 @@
 # @Email   : Vincent@163.com
 # @File    : sample.py
 # @Software: PyCharm
+
+def ex():
+    raise Exception("excepting")
+
+if __name__ == "__main__":
+    try:
+        print("123")
+        ex()
+        print("456")
+    except Exception as e:
+        print("789")
+        print(type(e))
+        print(e.__str__())
+    else:
+        print('没有出现异常')#没有异常时才会执行
+    finally:
+        print('资源回收')
+
+
+
