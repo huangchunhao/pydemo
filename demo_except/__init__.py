27d41b0a09d5145471b4e9c80b2da76b8711a8ba
huangchunhao <2015huangchunhao>
2019-08-18 21:30:16 +0800

    except

---
+++ demo_except/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2019/8/18 0018 17:39
+# @Author  : Vincent
+# @Email   : Vincent@163.com
+# @File    : __init__.py.py
+# @Software: PyCharm
---
+++ demo_except/sample.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2019/8/18 0018 17:47
+# @Author  : Vincent
+# @Email   : Vincent@163.com
+# @File    : sample.py
+# @Software: PyCharm
