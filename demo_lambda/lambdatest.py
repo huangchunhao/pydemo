37e806c324d5babd5f30af3e13c2fedfd88916f6
huangchunhao <huang.chunhao@hbfintech.com>
2019-08-19 09:37:41 +0800

    lambda

---
+++ demo_lambda/lambdatest.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2019/8/16 13:54
+# @Author  : Vincent
+# @Email   : Vincent@163.com
+# @File    : lambdatest.py
+# @Software: PyCharm
+import uuid
+
+a=lambda :str(uuid.uuid4())
+print(a())
+
+
+b=lambda x:str(uuid.uuid4())+str(x)
+print(b("xxxxxxx"))
+
