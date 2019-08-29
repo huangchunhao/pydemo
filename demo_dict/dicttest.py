cedabe102a9f1632d5f913768dc2310e8a7d4d44
huangchunhao <huang.chunhao@hbfintech.com>
2019-08-29 14:07:52 +0800

    dict

--- demo_dict/dicttest.py
+++ demo_dict/dicttest.py
@@ -5,3 +5,16 @@
 # @File    : dicttest.py
 # @Software: PyCharm
 
+dict={"code":123,"resp":"4565","data":{"id":5689},"list":[1,2,3,4,5],"listd":[{"id":5689},{"id":4545},{"id":5585}]}
+print(dict["code"])
+print(dict["resp"])
+print(dict["data"])
+print(dict["data"]["id"])
+print(dict["list"][0])
+print(dict["list"][4])
+print(dict["listd"][0]["id"])
+
+dict["code"]=5689
+print(dict)
+dict["code"]="8888"
+print(dict)
