60d401093b30d60b24b217c06216677181549dd4
huangchunhao <huang.chunhao@hbfintech.com>
2019-09-10 13:25:55 +0800

    turtle

--- demo_turtle/one.py
+++ demo_turtle/one.py
@@ -5,3 +5,18 @@
 # @File    : one.py
 # @Software: PyCharm
 
+import turtle
+
+turtle.showturtle() #显示箭头
+turtle.write("高淇") #写字符串
+turtle.forward(300) #前进 300 像素
+turtle.color("red") #画笔颜色改为 red
+turtle.left(90) #箭头左转 90 度
+turtle.forward(300)
+turtle.goto(0,50) #去坐标（0,50）
+turtle.goto(0,0)
+turtle.penup() #抬笔。这样，路径就不会画出来
+turtle.goto(0,300)
+turtle.pendown() #下笔。这样，路径就会画出来
+turtle.circle(100) #画圆
+
