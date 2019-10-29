d1668d7fde6bfa8d0f9cbf86f2f587c554a2f90a
huangchunhao <huang.chunhao@hbfintech.com>
2019-10-29 17:13:11 +0800

    2

--- demo_pytest/caplog/caplogtest.py
+++ demo_pytest/caplog/caplogtest.py
@@ -4,4 +4,16 @@
 # @Email   : Vincent@163.com
 # @File    : caplogtest.py
 # @Software: PyCharm
+import logging
 
+class TestClass(object):
+
+
+    def test_one(self,caplog):
+        logger = logging.getLogger(__name__)
+        logger.info('spam')
+        logger.warning('eggs')
+        logger.error('bacon')
+
+        x = "this"
+        assert 'i' in x
