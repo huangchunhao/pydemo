e2238ce251cf30720d3b2ea7124dae5430244ab1
huangchunhao <huang.chunhao@hbfintech.com>
2019-10-29 17:12:52 +0800

    20191029

---
+++ demo_pytest/caplog/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2019/10/29 15:14
+# @Author  : Vincent
+# @Email   : Vincent@163.com
+# @File    : __init__.py.py
+# @Software: PyCharm
+
---
+++ demo_pytest/caplog/caplogtest.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2019/10/29 15:15
+# @Author  : Vincent
+# @Email   : Vincent@163.com
+# @File    : caplogtest.py
+# @Software: PyCharm
+
