ea8fbdaabfbd0d9b226d9a01433c6db0957973c6
huangchunhao <2015huangchunhao>
2019-11-11 21:15:26 +0800

    快排

--- demo_pythonic/1quicksort.py
+++ demo_pythonic/1quicksort.py
@@ -4,3 +4,32 @@
 # @Email   : Vincent@163.com
 # @File    : 1quicksort.py
 # @Software: PyCharm
+
+def quicksort(array):
+    print("*"*20)
+    print("原始：")
+    print(array)
+    less=[];greater=[]
+    if len(array)<=1:
+        print("结果1：")
+        print(array)
+        return array
+    pivot=array.pop()
+    print(pivot)
+    for x in array:
+        if x<= pivot:
+            less.append(x)
+            # print("less:")
+            # print(less)
+        else:
+            greater.append(x)
+            # print("greater:")
+            # print(greater)
+    result=quicksort(less)+[pivot]+quicksort(greater)
+    print("结果2：")
+    print(result)
+    return result
+
+quicksort([9,8,4,5,32,64,2,1,0,10,19,27])
+
+#quicksort([9,8,4,5])
