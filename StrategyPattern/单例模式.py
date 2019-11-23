e45adaecb9dc500f886958c3362c1dc141f1775a
huangchunhao <2015huangchunhao>
2019-11-23 11:47:13 +0800

    设计模式

--- StrategyPattern/单例模式.py
+++ StrategyPattern/单例模式.py
@@ -4,3 +4,16 @@
 # @Email   : Vincent@163.com
 # @File    : 单例模式.py
 # @Software: PyCharm
+
+class Singleton(object):
+    def __init__(self):
+          pass
+
+    def __new__(cls, *args, **kwargs):
+        if not hasattr(Singleton, "_instance"): # 反射
+            Singleton._instance = object.__new__(cls)
+        return Singleton._instance
+
+obj1 = Singleton()
+obj2 = Singleton()
+print(obj1, obj2)
--- StrategyPattern/策略模式.py
+++ StrategyPattern/策略模式.py
@@ -4,3 +4,41 @@
 # @Email   : Vincent@163.com
 # @File    : 策略模式.py
 # @Software: PyCharm
+
+#https://www.cnblogs.com/ydf0509/p/8527515.html
+
+class People(object):
+    def sayJob(self):
+        pass
+
+
+class Student(People):
+    def __init__(self):
+        pass
+
+    def sayJob(self):
+        print("I am a {}".format("student"))
+
+class Teacher(People):
+    def __init__(self):
+        pass
+
+    def sayJob(self):
+        print("I am a {}".format("teacher"))
+
+
+#具体策略类
+class Factory(object):
+    def __init__(self,people):
+        self.people=people
+
+    def sayJob(self):
+        self.people.sayJob()
+
+
+
+f1=Factory(Student())
+f2=Factory(Teacher())
+
+f1.sayJob()
+f2.sayJob()
